83884a86eec7af546168309c643e79283c7715a2
abdalla <abdalla@pop-os.localdomain>
2022-08-31 11:26:55 +0200

    finished SAP.

--- week1/assigment/wordnet/SAP.java
+++ week1/assigment/wordnet/SAP.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
@@ -13,17 +12,14 @@ public class SAP {
   private BreadthFirstDirectedPaths BFS;
 
   SAP(Digraph GOr) {
+    if (GOr == null) {
+
+      throw new IllegalArgumentException("Digraph is null");
+    }
+    // doubleEdge digraph (normal graph);
     doubleEdge = new Digraph(GOr);
     G = new Digraph(GOr);
 
-    // for (int i = 0; i < doubleEdge.V(); i++) {
-    // System.out.println("constructor");
-    // System.out.print("V: " + i + " ");
-    // for (int w : G.adj(i)) {
-    // System.out.print(w + ", ");
-    // }
-    // System.out.println(" ");
-    // }
     for (int i = 0; i < doubleEdge.V(); i++) {
       for (int w : doubleEdge.adj(i)) {
         doubleEdge.addEdge(w, i);
@@ -32,6 +28,9 @@ public class SAP {
   }
 
   public int length(int v, int w) {
+
+    nullChecker(v);
+    nullChecker(w);
 
     BFS = new BreadthFirstDirectedPaths(doubleEdge, v);
     int t = BFS.distTo(w);
@@ -42,35 +41,15 @@ public class SAP {
 
   public int ancestor(int v, int w) {
 
+    nullChecker(v);
+    nullChecker(w);
+
     BFS = new BreadthFirstDirectedPaths(doubleEdge, v);
     int t = BFS.distTo(w);
     if (t == Integer.MAX_VALUE)
       return -1;
     else {
       return ancestorHelper(w, BFS);
-      // what i do here is i match the path to the original digraph.
-      // ArrayList<Integer> path = new ArrayList<>();
-      // for (int pathway : BFS.pathTo(w)) {
-      // path.add(pathway);
-      // // System.out.print(pathway + " -> ");
-
-      // }
-      // // System.out.println(" ");
-      // for (int pathway = 0; pathway < path.size() - 1; pathway++) {
-      // boolean found = false;
-      // // System.out.print(path.get(pathway) + ": ");
-
-      // for (int parent : G.adj(path.get(pathway))) {
-      // // System.out.print(parent + ", ");
-      // if (parent == path.get(pathway + 1)) {
-      // found = true;
-      // break;
-      // }
-      // }
-      // if (!found) {
-      // return path.get(pathway);
-      // }
-      // }
     }
   }
 
@@ -79,11 +58,6 @@ public class SAP {
     int past = 0;
     boolean first = true;
 
-    // for (int pathway : BFS.pathTo(p)) {
-
-    // System.out.print(pathway + "- > ");
-
-    // }
     for (int pathway : BFS.pathTo(p)) {
       if (first) {
         past = pathway;
@@ -102,10 +76,13 @@ public class SAP {
       }
       past = pathway;
     }
-    return 10000000;
+    return p;
   };
 
   public int length(Iterable<Integer> v, Iterable<Integer> w) {
+    iterableNullChecker(v);
+    iterableNullChecker(w);
+
     int small = Integer.MAX_VALUE;
     BFS = new BreadthFirstDirectedPaths(doubleEdge, v);
 
@@ -127,20 +104,20 @@ public class SAP {
   }
 
   public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+    iterableNullChecker(v);
+    iterableNullChecker(w);
+
     int small = Integer.MAX_VALUE;
     BFS = new BreadthFirstDirectedPaths(doubleEdge, v);
 
     int to = 0;
     for (int W : w) {
-      // System.out.print("shortest to: " + W + " is: ");
       int V = BFS.distTo(W);
-      // System.out.print(V + ", ");
 
       if (V < small) {
         small = V;
         to = W;
       }
-      // System.out.println(" ");
     }
 
     if (small == Integer.MAX_VALUE) {
@@ -157,17 +134,30 @@ public class SAP {
     return ancestorHelper(temp, BFS);
   }
 
+  void nullChecker(Integer o) {
+    if (o == null)
+      throw new IllegalArgumentException("null is passed");
+  }
+
+  void iterableNullChecker(Iterable<Integer> it) {
+    for (Integer integer : it) {
+      if (integer == null) {
+        throw new IllegalArgumentException("null is passed throw iterable");
+      }
+    }
+  }
+
   public static void main(String[] args) {
-    In in = new In("digraph25.txt");
+    In in = new In("digraph1.txt");
     Digraph G = new Digraph(in);
     SAP sap = new SAP(G);
-    // while (!StdIn.isEmpty()) {
-    // int v = StdIn.readInt();
-    // int w = StdIn.readInt();
-    // int length = sap.length(v, w);
-    // int ancestor = sap.ancestor(v, w);
-    // StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-    // }
+    while (!StdIn.isEmpty()) {
+      int v = StdIn.readInt();
+      int w = StdIn.readInt();
+      int length = sap.length(v, w);
+      int ancestor = sap.ancestor(v, w);
+      StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+    }
 
     ArrayList<Integer> arr1 = new ArrayList<>();
     arr1.add(13);
@@ -175,7 +165,7 @@ public class SAP {
     arr1.add(24);
     ArrayList<Integer> arr2 = new ArrayList<>();
     arr2.add(6);
-    arr2.add(16);
+    arr2.add(null);
     arr2.add(17);
 
     System.out.println(sap.length(arr1, arr2));
--- week1/assigment/wordnet/WordNet.java
+++ week1/assigment/wordnet/WordNet.java
@@ -6,50 +6,50 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.ArrayList;
 
 public class WordNet {
-    private Digraph G;
-    private ArrayList<String> synList;
+  private Digraph G;
+  private ArrayList<String> synList;
+  private int SynLength;
 
-    public WordNet(String synsets, String hypernyms) {
+  public WordNet(String synsets, String hypernyms) {
 
+    synList = new ArrayList<>();
+    In synSets = new In(synsets);
+    In hypernymsSet = new In(hypernyms);
 
-        synList = new ArrayList<>();
-        In synSets = new In(synsets);
-        In hypernymsSet = new In(hypernyms);
+    while (synSets.hasNextLine()) {
+      String[] nouns = synSets.readLine().split(",");
+      String[] noun = nouns[1].split(" ");
+      for (String s : noun) {
 
-        while (synSets.hasNextLine()) {
-            String[] nouns = synSets.readLine().split(",");
-            String[] noun = nouns[1].split(" ");
-            for (String s: noun) {
-
-                synList.add(s);
-            }
-        }
-
-        G = new Digraph(synList.size());
-        while (hypernymsSet.hasNextLine()) {
-            String[] hyper = hypernymsSet.readLine().split(",");
-            for (int i = 1; i < hyper.length; i++) {
-                G.addEdge(Integer.parseInt(hyper[0]), Integer.parseInt(hyper[i]));
-            }
-        }
+        synList.add(s);
+      }
     }
 
-    public Iterable<String> nouns() {
-        return synList;
+    G = new Digraph(synList.size());
+    while (hypernymsSet.hasNextLine()) {
+      String[] hyper = hypernymsSet.readLine().split(",");
+      for (int i = 1; i < hyper.length; i++) {
+        G.addEdge(Integer.parseInt(hyper[0]), Integer.parseInt(hyper[i]));
+      }
+    }
+  }
+
+  public Iterable<String> nouns() {
+    return synList;
+  }
+
+  public boolean isNoun(String word) {
+    return true;
+  }
+
+  public static void main(String[] args) {
+
+    WordNet w = new WordNet("synsets.txt", "hypernyms15Tree.txt");
+    // WordNet w = new WordNet("synsets15.txt", "hypernyms15Tree.txt");
+    for (String s : w.nouns()) {
+      StdOut.println(s);
+
     }
 
-    public boolean isNoun(String word) {
-        return true;
-    }
-
-    public static void main(String[] args) {
-
-        WordNet w = new WordNet("synsets.txt", "hypernyms15Tree.txt");
-        // WordNet w = new WordNet("synsets15.txt", "hypernyms15Tree.txt");
-        for (String s : w.nouns()) {
-            StdOut.println(s);
-
-        }
-
-    }
+  }
 }
